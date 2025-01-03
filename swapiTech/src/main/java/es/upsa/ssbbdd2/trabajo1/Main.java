baf57d2ba1abd9a2c110750034eab05d1bdc1bcd
Gonzaloogm <ggardonma.inf@upsa.es>
2025-01-03 13:23:39 +0100

    Commit 1

---
+++ swapiTech/src/main/java/es/upsa/ssbbdd2/trabajo1/Main.java
@@ -0,0 +1,31 @@
+package es.upsa.ssbbdd2.trabajo1;
+
+import es.upsa.ssbbdd2.trabajo1.swapi.StarWarsApi;
+import es.upsa.ssbbdd2.trabajo1.swapi.impl.StarWarsApiImpl;
+
+
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        try ( StarWarsApi swapi = new StarWarsApiImpl() )
+        {
+           generateFilms(swapi);
+           generatePeople(swapi);
+        }
+    }
+
+    static void generateFilms(StarWarsApi swapi) throws Exception
+    {
+        // swapi.request( StarWarsApi.FILMS_URL )
+
+    }
+
+    static void generatePeople(StarWarsApi swapi) throws Exception
+    {
+        // swapi.request( StarWarsApi.PEOPLE_URL )
+
+    }
+
+}
